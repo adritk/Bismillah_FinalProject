3f7569e7986c66f0d24884e42425bd88b346d05e
adritk <adirscatk@gmail.com>
2020-02-12 07:59:19 +0700

    commit2

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,13 +1,31 @@
 import React from 'react';
-import Footerinfo from '../component/Footerinfo'
 import '../style/footer.css'
-
-
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import PhoneIcon from '@material-ui/icons/Phone';
+import EmailIcon from '@material-ui/icons/Email';
 
 const Footer = () => {
     return (
-      <div>
-           <Footerinfo address="BSD Green Office GOP 9 - G Floor, BSD City Tangerang Banten" phone="085156240856"/>
+      <div className="footer">
+        <div className="row-1">
+            <h3>Kontak Kami</h3>
+            <div className="logoInfo">
+                <LocationOnIcon />
+                    <div className="textInfo">
+                        <span>BSD Green Office GOP 9 - G Floor, BSD City Tangerang Banten</span>
+                    </div>
+            </div>
+
+            <div className="logoPhone">
+                <PhoneIcon /> 
+                <span> &nbsp; 085156240856</span>
+            </div>
+
+            <div className="logoEmail">
+                <EmailIcon /> 
+                <span>&nbsp; &nbsp;Adirsca@travelindo.com</span>
+            </div>
+        </div>
       </div>
     )
 }
