01830c5c96edf9d155373e3d62e24bae857d2088
adritk <adirscatk@gmail.com>
2020-02-21 06:53:29 +0700

    edit delete admin

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -9,23 +9,19 @@ class AdminPage extends Component {
         openModal : false,
         addImageFileName : 'Select File',
         addImageFile : undefined,
-        showImage : null
+        showImage : null,
+        selectedId : null
     }
 
     componentDidMount() {
-        // Axios.get(API_URL + '/getpackage')
-        // .then((res) => {
-        //     console.log(res.data)
-        //     this.setState({product : res.data})
-        // })
-        this.getProduct()
+        this.getPackage()
     }
 
 
-    getProduct = () => {
+    getPackage = () => {
         Axios.get(API_URL + '/getpackage')
         .then((res) => {
-            console.log(res.data)
+            // console.log(res.data)
             this.setState({product : res.data})
         })
         .catch((err) => {
@@ -42,7 +38,7 @@ class AdminPage extends Component {
         }
     }
 
-    UploadProduct = () => {
+    UploadPackage = () => {
         let { addImageFile } = this.state;
         console.log(addImageFile)
         if(addImageFile){
@@ -65,10 +61,10 @@ class AdminPage extends Component {
             console.log(formData)
             Axios.post(API_URL + '/addpackage', formData)
             .then((res) => {
-                console.log(res)
+                // console.log(res)
                 alert('add succesfull')
-                this.setState({openModal : false, showImage : null, addImageFile : null})
-                this.getProduct()  
+                this.setState({openModal : false, showImage : null, addImageFile : undefined})
+                this.getPackage()  
             })
             .catch((err) => {
                 console.log(err)
@@ -79,65 +75,137 @@ class AdminPage extends Component {
                         alert('isi semua kolom')
                     }
     }
+
+    editPackage = (id) => {
+        let title = this.title.value
+        let harga = this.harga.value
+        let description = this.description.value
+        let location = this.location.value
+        let duration = this.duration.value
+        let product = {
+            title,
+            harga,
+            description,
+            location,
+            duration
+        }
+        Axios.put(API_URL + `/editpackage/${id}`, product)
+        .then((res) => {
+            // console.log(res.data)
+            alert('edit successfull')
+            this.getPackage()
+            this.setState({selectedId : null})
+
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
+
     
+   deletePackage = (id) => {
+        Axios.delete(API_URL + `/deletepackage/${id}`)
+        .then((res) => {
+            alert('delete successfull')
+            this.getPackage()
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+   }
 
-    // addProduct = () => {
-    //     let title = this.title.value
-    //     let harga = this.harga.value
-    //     let description = this.description.value
-    //     let location = this.location.value
-    //     let duration = this.duration.value
-    //     let product = {
-    //         title,
-    //         harga,
-    //         description,
-    //         location,
-    //         duration
-    //     }
-    //     if (title && harga && description && location && duration ) {
-    //         Axios.post(API_URL + `/addpackage`, product)
-    //         .then((res) => {
-    //             console.log(res.data)
-    //             this.getProduct()   
-    //         })
-    //         .catch((err) => {
-    //             console.log(err)
-    //         })
-    //     }
-            
-    // }
+    renderPackage = () => {
+        let {product, selectedId} = this.state
+            return product.map((item, index) => {
+                if(item.id === selectedId) {
 
-    renderProduct = () => {
-        return this.state.product.map((item, index) => {
-            return (
-                <TableRow>
-                <TableCell>
-                    {item.id}
-                </TableCell>
-                <TableCell>
-                    {item.title}
-                </TableCell>
-                <TableCell>
-                   Rp. {item.harga}
-                </TableCell>
-                <TableCell>
-                    {item.description}
-                </TableCell>
-                <TableCell>
-                   {item.location}
-                </TableCell>
-                <TableCell>
-                  {item.duration}
-                </TableCell>
-                <TableCell>
-                  <img src={API_URL + '/' + item.imagePath} style={{width: "50px", height: "50px"}} alt="notfound" />
-                </TableCell>
-                <TableCell>
-                         <Button color="warning" onClick={() => this.setState({selectedId: (item.id)})}>Edit</Button>
-                         <Button color="danger" onClick={() => this.deleteMovie(item.id)}>Delete</Button>
-                     </TableCell>
-            </TableRow>
-            )
+                    return (
+                        <TableRow>
+                                <TableCell>
+                                    {item.id}
+                                </TableCell>
+
+                                <TableCell>
+                                    <Input defaultValue= {item.title} 
+                                           innerRef = {(title) => this.title = title}
+                                           />
+                                </TableCell>
+
+                                <TableCell>
+                                    <Input defaultValue={item.harga}
+                                           innerRef = {(harga) => this.harga= harga}
+                                           /> 
+                                </TableCell>
+
+                                <TableCell>
+                                    <Input defaultValue={item.description}
+                                           innerRef= {(description) => this.description = description}
+                                           />
+                                </TableCell>
+
+                                <TableCell>
+                                    <Input defaultValue= {item.location}
+                                           innerRef= {(location) => this.location = location}
+                                           />
+                                </TableCell>
+
+                                <TableCell>
+                                    <Input defaultValue= {item.duration}
+                                           innerRef = {(duration) => this.duration = duration}
+                                           />
+                                </TableCell>
+
+                                <TableCell>
+                                    <img src={API_URL + '/' + item.imagePath} style={{width: "50px", height: "50px"}} alt="notfound" />
+                                </TableCell>
+
+                                <TableCell>
+                                    <Button color="warning" onClick={() => this.setState({selectedId: null})}>
+                                        Cancel
+                                    </Button>
+                                    <Button color="danger" onClick={() => this.editPackage(item.id)}>
+                                        Confirm
+                                    </Button>
+                                </TableCell>
+                        </TableRow>
+                    )
+                }
+                    return (
+                        <TableRow>
+                                <TableCell>
+                                    {item.id}
+                                </TableCell>
+
+                                <TableCell>
+                                    {item.title}         
+                                </TableCell>
+
+                                <TableCell>
+                                    {item.harga}
+                                </TableCell>
+
+                                <TableCell>
+                                    {item.description}                             
+                                </TableCell>
+
+                                <TableCell>
+                                    {item.location}       
+                                </TableCell>
+
+                                <TableCell>
+                                   {item.duration}
+                                </TableCell>
+
+                                <TableCell>
+                                    <img src={API_URL + '/' + item.imagePath} style={{width: "50px", height: "50px"}} alt="notfound" />
+                                </TableCell>
+
+                                <TableCell>
+                                    <Button color="warning" onClick={() => this.setState({selectedId: (item.id)})}>Edit</Button>
+                                    <Button color="danger" onClick={() => this.deletePackage(item.id)}>Delete</Button>
+                                </TableCell>
+                        </TableRow>
+                    )
         })
     }
 
@@ -188,11 +256,7 @@ class AdminPage extends Component {
                             <Label>
                                 Image
                             <Input 
-                            onChange={this.onBtnUploadFile} label={this.state.addImageFileName} type='file'/>
-                              {/* <Button onClick={this.uploadImage}  //func upload product
-                              style={{width: '100px', height: "30px", padding: '0px', marginLeft: '0px'}}>
-                                 Upload 
-                              </Button> */}
+                            onChange={this.onBtnUploadFile} label={this.state.addImageFileName} type='file' style={{width: "100%"}}/>
                             </Label>
 
                             <div style={{marginTop:'25px', float: 'right', width: "162px", height:"90px"}}>
@@ -213,8 +277,8 @@ class AdminPage extends Component {
                         </ModalBody>
 
                         <ModalFooter>
-                        <Button color="primary" onClick={this.UploadProduct}>Confirm</Button>
-                        <Button color="warning" onClick={() => this.setState({openModal:false,addImageFile: null, showImage: null})}>Cancel</Button>
+                        <Button color="primary" onClick={this.UploadPackage}>Confirm</Button>
+                        <Button color="warning" onClick={() => this.setState({openModal:false,addImageFile: undefined, showImage: null})}>Cancel</Button>
                         </ModalFooter>
                 </Modal>
                 <br></br>
@@ -230,7 +294,7 @@ class AdminPage extends Component {
                             <TableCell>Actions</TableCell>
                     </TableHead>
                     <TableBody>
-                        {this.renderProduct()}
+                        {this.renderPackage()}
                     </TableBody>
                 </Table>
             </div>
